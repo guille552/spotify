608d41f90ae8d916de3b2013da5b259293a61b4b
guille552 <guillermo.medina021102@gmial.com>
2023-12-03 20:20:30 -0600

    spotify

---
+++ src/Global/Global.js
@@ -0,0 +1,12 @@
+var Global = {
+	playlistLimit: 50,
+	songLimit: 100,
+	access_token: "",
+	refresh_token: "",
+	redirect_uri : "http://localhost:3000/",
+	client_id : "ae8a512d82a744399ab3c26d9f3f7032",
+	client_secret : "0d076cf3819a4884842d68cb0b923661",
+	scopes : "user-read-private user-read-email playlist-read-private"
+}
+
+export default Global;
